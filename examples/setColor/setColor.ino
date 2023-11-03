babedb038ace82c3cc29db845dac6cf68d929b31
Erik van Roode <evanroode@users.noreply.github.com>
2023-11-02 20:06:36 -0700

    Update setColor.ino
    I suspect this string was meant to be "color"

--- examples/setColor/setColor.ino
+++ examples/setColor/setColor.ino
@@ -40,7 +40,7 @@ void setup() {
     // set up the LCD's number of columns and rows:
     lcd.begin(16, 2);
     // Print a message to the LCD.
-    lcd.print("set cllor");
+    lcd.print("set color");
 }
 
 void loop() {
